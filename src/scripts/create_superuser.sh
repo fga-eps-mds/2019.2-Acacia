6e17495eb5c9ca1b87a9adbc4a56fafb3c78936e
Durval Carvalho <durvalcsouza@outlook.com>
2019-09-24 16:00:34 -0300

    #43 Add script to create default superuser

---
+++ src/scripts/create_superuser.sh
@@ -0,0 +1,4 @@
+# default superuser
+# email: 'admin@admin.com'
+# password: 'password'
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser(username='admin', email='admin@admin.com', password='password')" | python manage.py shell
