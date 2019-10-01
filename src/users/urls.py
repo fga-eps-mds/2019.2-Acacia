c2e71a5805683027388afb05f7665aa317143a7d
Hugo Sobral <hugosobral10@gmail.com>
2019-09-30 22:19:19 -0300

    #43 Add tokens lib
    Co-authored-by: Leonardo da Silva Gomes <leonardodasigomes@gmail.com>

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,10 +1,19 @@
-from django.urls import path
+# Django
+from django.urls import path, include
+
+# Viewsets
 from .viewsets import UserRegistrationAPIView
 
-from rest_framework.authtoken.views import obtain_auth_token
+# Simple JWT
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 app_name = 'users'
 
 urlpatterns = [
     path('signup/', UserRegistrationAPIView.as_view(), name='signup'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
