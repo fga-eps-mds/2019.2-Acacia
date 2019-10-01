740253b72c7c522cede7d9434ea792d33cbbf7c9
Hugo Sobral <hugosobral10@gmail.com>
2019-10-01 17:10:30 -0300

    #43 Fix merge conflicts
    Co-authored-by: Leonardo da Silva Gomes <leonardodasigomes@gmail.com>

--- src/users/urls.py
+++ src/users/urls.py
@@ -13,11 +13,7 @@ from rest_framework_simplejwt.views import (
 app_name = 'users'
 
 urlpatterns = [
-<<<<<<< HEAD
     path('signup/', UserRegistrationAPIView.as_view(), name='signup'),
-=======
-    path('signup/', UserRegistrationAPIView.as_view(), name='register'),
->>>>>>> f31e1452b216abcc05a194a3985b768a7eff4b1e
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
