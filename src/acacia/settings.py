9b448fac87f39531ea84615a2290ce3432c7c5e2
Fabíola Fleury <fabiola.mfleury@gmail.com>
2019-10-01 22:28:46 -0300

    #43 Try to fix database connection trouble

--- src/acacia/settings.py
+++ src/acacia/settings.py
@@ -14,6 +14,8 @@ import os
 from scripts.wait_for_db import start_services
 from django.utils.translation import ugettext_lazy as _
 
+from .wait_db import start_services
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -93,6 +95,7 @@ DATABASES = {
     }
 }
 
+# STARTS SERVICES THAT DJANGO DEPENDS E.G. postgres
 start_services()
 
 LANGUAGES = (
