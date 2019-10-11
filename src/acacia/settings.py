18cb249d54ef1d289be4f9c4d0ffef22b30dcddc
jps12 <jps12carvalho@gmail.com>
2019-10-11 15:48:12 -0300

    #73 Add profile model and fix docker-compose
    Signed-off-by: jps12 <jps12carvalho@gmail.com>
    Co-authored-by: Fabíola <fabiola.mfleury@gmail.com>

--- src/acacia/settings.py
+++ src/acacia/settings.py
@@ -104,6 +104,8 @@ LANGUAGES = (
     ('fr-CA', _('French Canadian')),
 )
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
