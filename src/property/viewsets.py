3dc0258713cfb7470c019bea14f1ed9adf10b351
Durval Carvalho <durvalcsouza@outlook.com>
2019-10-12 00:36:17 -0300

    #66 Allow read-only permission for unregistered users

--- src/property/viewsets.py
+++ src/property/viewsets.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 
@@ -9,13 +9,32 @@ from users.models import User
 
 from .models import Property
 from .serializers import PropertySerializer
-from .permissions import UserIsOwnerProperty
+from .permissions import UserIsPropertyOwner
+
+from localflavor.br.br_states import STATE_CHOICES
 
 class PropertyListCreateAPIView(generics.ListCreateAPIView):
     serializer_class =  PropertySerializer
+    permission_classes = (IsAuthenticatedOrReadOnly, )
 
+    # only this function is allowed without authentication
     def get(self, request, *args, **kwargs):
-        return Response({'test':'test'})
+        """
+        This endpoint returns an empty json with the 
+        parameters needed to create a new user
+        """
+
+        required_fields = {'meta': 'the following fields are required'}
+        required_fields.update(PropertySerializer().data)
+
+        # this field only accepts values from this list
+        options = [key for key, value in Property.TYPE_OF_ADDRESS]
+        required_fields['type_of_address'] = {"options" : options}
+        
+        options = [key for key, value in STATE_CHOICES]
+        required_fields['state'] = {"options" : options}
+
+        return Response(required_fields)
 
     def get_queryset(self):
         return Property.objects.filter(owner=self.request.user.id)
@@ -27,8 +46,7 @@ class PropertyListCreateAPIView(generics.ListCreateAPIView):
 class PropertyDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class =  PropertySerializer
     queryset = Property.objects.all()
-    permission_classes = (UserIsOwnerProperty, )
+    permission_classes = (IsAuthenticated, UserIsPropertyOwner)
 
     def get_queryset(self):
         return Property.objects.filter(owner=self.request.user.id)
-
