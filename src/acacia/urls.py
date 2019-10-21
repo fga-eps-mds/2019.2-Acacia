c3e001098efdfb4432dc4dd78682a64d4b57a86e
Durval Carvalho <durvalcsouza@outlook.com>
2019-10-20 23:51:13 -0300

    #66 Change route property to properties

--- src/acacia/urls.py
+++ src/acacia/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
-    path('property/', include('property.urls')),
+    path('properties/', include('property.urls')),
 ]
 
 from .helpers import list_all_endpoints
