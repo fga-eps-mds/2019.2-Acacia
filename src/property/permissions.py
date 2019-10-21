f193aa25bd0487c8e513d16196ad50354fc05a43
Durval Carvalho <durvalcsouza@outlook.com>
2019-10-20 21:52:00 -0300

    #66 Change Property viewset inheritance to ModelViewSet
    Co-authored-by: Hugo Sobral <hugosobral10@gmail.com>

--- src/property/permissions.py
+++ src/property/permissions.py
@@ -1,5 +1,14 @@
 from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 
-class UserIsPropertyOwner(BasePermission):
+class UserIsPropertyOwner(permissions.BasePermission):
+    """
+    Object-level permission to only allow owners of an object to edit it.
+    Assumes the model instance has an `user` attribute.
+    """
+
     def has_object_permition(self, request, view, property):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+            
         return request.user.id == property.owner.id
--- src/property/viewsets.py
+++ src/property/viewsets.py
@@ -1,52 +1,29 @@
-from django.shortcuts import render
-
-from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 
+
 from users.models import User
 
-from .models import Property
 from .serializers import PropertySerializer
 from .permissions import UserIsPropertyOwner
+from .models import Property
 
-from localflavor.br.br_states import STATE_CHOICES
 
-class PropertyListCreateAPIView(generics.ListCreateAPIView):
+class PropertyViewSet(ModelViewSet):
     serializer_class =  PropertySerializer
-    permission_classes = (IsAuthenticated, )
-
-    # only this function is allowed without authentication
-    def get(self, request, *args, **kwargs):
-        """
-        This endpoint returns an empty json with the 
-        parameters needed to create a new user
-        """
-
-        required_fields = {'meta': 'the following fields are required'}
-        required_fields.update(PropertySerializer().data)
-
-        # this field only accepts values from this list
-        options = [key for key, value in Property.TYPE_OF_ADDRESS]
-        required_fields['type_of_address'] = {"options" : options}
-        
-        options = [key for key, value in STATE_CHOICES]
-        required_fields['state'] = {"options" : options}
-
-        return Response(required_fields)
+    queryset = Property.objects.all()
+    
+    permission_classes = (
+        IsAuthenticated, 
+        UserIsPropertyOwner
+    )
 
     def get_queryset(self):
-        return Property.objects.filter(owner=self.request.user.id)
+        return Property.objects.filter(
+            owner=self.request.user.id
+        )
     
     def perform_create(self, serializers):
         owner = User.objects.get(pk = self.request.user.id)
         serializers.save(owner = owner)
-    
-class PropertyDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
-    serializer_class =  PropertySerializer
-    queryset = Property.objects.all()
-    permission_classes = (IsAuthenticated, UserIsPropertyOwner)
-
-    def get_queryset(self):
-        return Property.objects.filter(owner=self.request.user.id)
