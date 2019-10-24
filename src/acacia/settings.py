f07a0645467c195235913e703a07cae74b10c2b4
Vítor Xoteslem <vitorcx010@gmail.com>
2019-10-23 21:56:23 -0300

    #79 Remove unused db script and ajust wait_db
    Co-authored-by: Shayane <shayaneliebe@gmail.com>

--- src/acacia/settings.py
+++ src/acacia/settings.py
@@ -11,9 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-from scripts.wait_for_db import start_services
 from django.utils.translation import ugettext_lazy as _
-
 from .wait_db import start_services
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
