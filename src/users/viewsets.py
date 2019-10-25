cd5dc1fb17dae04ad0187ef899614f2724505546
Shayane Alcantara <shayaneliebe@gmail.com>
2019-10-25 15:21:19 -0300

    Fix devel

--- src/users/viewsets.py
+++ src/users/viewsets.py
@@ -1,6 +1,6 @@
 from rest_framework import permissions
 from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
 from rest_framework.permissions import IsAuthenticated
 
