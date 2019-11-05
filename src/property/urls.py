be1cd2c43e99398cb58289efb046e4823ed45a82
Durval Carvalho <durvalcsouza@outlook.com>
2019-11-05 16:09:28 -0300

    #67 Remove Property DefaultRouter
    Co-authored-by: Fabíola Fleury <fabiola.mfleury@gmail.com>

--- src/property/urls.py
+++ src/property/urls.py
@@ -1,12 +1,28 @@
+from django.urls import path, include
 from .viewsets import PropertyViewSet
 from rest_framework.routers import DefaultRouter
 
-router = DefaultRouter()
-router.register(r'', PropertyViewSet, base_name='property')
 
 app_name = 'property'
 
 urlpatterns = [
-]
+    path(
+        '',
+        PropertyViewSet.as_view({
+            'get': 'list',
+            'post':'create'
+        })
+    ),
 
-urlpatterns += router.urls
+    path('<int:pk>/trees/', include('tree.urls')),
+
+    path(
+        '<int:pk>/',
+        PropertyViewSet.as_view({
+            'delete': 'destroy',
+            'get': 'retrieve',
+            'patch': 'partial_update',
+            'put': 'update',
+        })
+    ),
+]
