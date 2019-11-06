e544ad9905e619dc6448c89bf3a4f8b76b0c0937
Durval Carvalho <durvalcsouza@outlook.com>
2019-11-06 15:45:22 -0300

    #67 Remove tree routers
    Co-authored-by: Fabíola Fleury <fabiola.mfleury@gmail.com>

--- src/acacia/urls.py
+++ src/acacia/urls.py
@@ -1,19 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
-from .helpers import list_all_endpoints
 from django.conf import settings
 from django.conf.urls.static import static
-from tree.viewsets import TreeViewSet
 
-
-router = routers.DefaultRouter()
-router.register(r'tree', TreeViewSet)
-
-from .helpers import list_all_endpoints
+# from .helpers import list_all_endpoints
 
 urlpatterns = [
-    path('', include(router.urls)), 
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('properties/', include('property.urls')),
@@ -21,7 +13,7 @@ urlpatterns = [
 
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT
-    
-urlpatterns = list_all_endpoints(urlpatterns)
+
+# urlpatterns = list_all_endpoints(urlpatterns)
