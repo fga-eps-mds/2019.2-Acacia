40d20f1abc4e7ee5ec0d17d4be5bf978df4e532f
Durval Carvalho <durvalcsouza@outlook.com>
2019-11-06 16:24:03 -0300

    #67 Fix flake8 errors
    Co-authored-by: Fabíola Fleury <fabiola.mfleury@gmail.com>

--- src/acacia/urls.py
+++ src/acacia/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
 # from .helpers import list_all_endpoints
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,7 +13,14 @@ urlpatterns = [
 
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
+
+    # urlpatterns += static(
+    #     settings.STATIC_URL,
+    #     document_root=settings.STATIC_ROOT
+    # )
 
 # urlpatterns = list_all_endpoints(urlpatterns)
