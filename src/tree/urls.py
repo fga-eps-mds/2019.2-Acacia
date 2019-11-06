e6390158f126b398c80eafd4e9480fc40745aa18
Durval Carvalho <durvalcsouza@outlook.com>
2019-11-06 16:17:47 -0300

    #67 Add Simple Router to Trees and HarvestMonth
    Co-authored-by: Fabíola Fleury <fabiola.mfleury@gmail.com>

---
+++ src/tree/urls.py
@@ -0,0 +1,23 @@
+from .viewsets import TreeViewSet, HarvestMonthViewSet
+from django.urls import path, include
+from rest_framework import routers
+
+app_name = 'tree'
+
+tree_router = routers.SimpleRouter()
+tree_router.register(r'', TreeViewSet, basename='tree')
+
+harvest_month_router = routers.SimpleRouter()
+harvest_month_router.register(
+    r'',
+    HarvestMonthViewSet,
+    basename='harvest_months'
+)
+
+urlpatterns = [
+    path(
+        '<int:pk_tree>/harvest_months/',
+        include(harvest_month_router.urls),
+    ),
+
+] + tree_router.urls
