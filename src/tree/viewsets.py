4c7ce563ddf8c5ba84201dad24cd5e582bed1702
Durval Carvalho <durvalcsouza@outlook.com>
2019-11-06 16:21:40 -0300

    #67 Add ModelViewSet to Tree and HarvestMonth
    Co-authored-by: Fabíola Fleury <fabiola.mfleury@gmail.com>

--- src/tree/viewsets.py
+++ src/tree/viewsets.py
@@ -1,15 +1,45 @@
-from rest_framework.viewsets import ModelViewSet
-from tree.models import Tree
-from tree.serializers import TreeSerializer
-from rest_framework.response import Response
-from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.filters import SearchFilter
+from rest_framework.viewsets import ModelViewSet
+from tree.models import Tree, HarvestMonth
+from property.models import Property
+from tree import serializers
+
 
 class TreeViewSet(ModelViewSet):
-    
     queryset = Tree.objects.all()
-    serializer_class = TreeSerializer
-    
-    # permission_class = (IsAuthenticated,)
-  
+    serializer_class = serializers.TreeSerializer
+    permission_class = (IsAuthenticated,)
+    lookup_field = 'pk_tree'
+
+    def perform_create(self, serializer):
+
+        property = Property.objects.get(
+            pk_property=self.kwargs['pk_property']
+        )
+
+        serializer.save(pk_property=property)
+
+    def get_queryset(self):
+        return Tree.objects.filter(
+            pk_property=self.kwargs['pk_property']
+        )
+
+
+class HarvestMonthViewSet(ModelViewSet):
+    queryset = HarvestMonth.objects.all()
+    serializer_class = serializers.HarvestMonthSerializer
+    permission_class = (IsAuthenticated,)
+    lookup_field = 'pk_harvest_month'
+
+    def perform_create(self, serializer):
+
+        tree = Tree.objects.get(
+            pk_tree=self.kwargs['pk_tree']
+        )
+
+        serializer.save(pk_tree=tree)
+
+    def get_queryset(self):
+        return HarvestMonth.objects.filter(
+            pk_tree=self.kwargs['pk_tree']
+        )
