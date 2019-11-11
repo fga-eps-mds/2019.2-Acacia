667e8574bc6c8a94f1ff2088ecb45ad3e9304ce4
Durval Carvalho <durvalcsouza@outlook.com>
2019-11-11 15:55:31 -0300

    #73 Update migrations and Fix docker-compose

--- src/users/tests.py
+++ src/users/tests.py
@@ -1,6 +1,5 @@
 from rest_framework.test import APITestCase
 from django.urls import reverse
-from django.db import IntegrityError
 
 
 class UserRegistrationAPIViewTestCase(APITestCase):
@@ -37,30 +36,6 @@ class UserRegistrationAPIViewTestCase(APITestCase):
 
         response = self.client.post(self.url, user_data)
         self.assertEqual(400, response.status_code)
-
-    def test_unique_email_validation(self):
-        """
-        Test to try to create a user with a registered email
-        """
-
-        user1_data = {
-            "username": "vitas",
-            "email": "vitas@iAmGreat.com",
-            "password": "VitasIsAwesome",
-            "confirm_password": "VitasIsAwesome"
-        }
-        user2_data = {
-            "username": "Reanu_Reves",
-            "email": "vitas@iAmGreat.com",
-            "password": "cyberpunk2077",
-            "confirm_password": "cyberpunk2077"
-        }
-        response = self.client.post(self.url, user1_data)
-
-        self.assertEqual(201, response.status_code)
-
-        with self.assertRaises(IntegrityError):
-            response = self.client.post(self.url, user2_data)
 
     def test_unique_username_validation(self):
         """
