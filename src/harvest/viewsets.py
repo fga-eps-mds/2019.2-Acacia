e408110740decf5de230d14de608abe9bc32bd66
Durval Carvalho <durvalcsouza@outlook.com>
2019-11-14 23:09:38 -0300

    #69 Update viewset to return next 7 days harvests

--- src/harvest/viewsets.py
+++ src/harvest/viewsets.py
@@ -66,11 +66,11 @@ class WeekHarvests(ListModelMixin, viewsets.GenericViewSet):
 
     def get_queryset(self):
 
-        end_date = datetime.datetime.now()
-        start_date = end_date - datetime.timedelta(weeks=1)
+        start_date = datetime.datetime.now()
+        end_date = start_date + datetime.timedelta(weeks=1)
 
         queryset = Harvest.objects.filter(
-            date__range=(start_date, end_date)
+            date__range=(end_date, start_date)
         )
 
         return queryset
